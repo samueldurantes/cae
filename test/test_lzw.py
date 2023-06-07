5670c63620d5510be440dd811dde92d5643cf480
Samuel Durante <samueldurante.h@gmail.com>
2023-06-07 00:27:27 +0000

    return string instead numbers list in encode function

--- test/test_lzw.py
+++ test/test_lzw.py
@@ -3,11 +3,9 @@ from zaf.lzw import Lzw
 
 class TestLzw(unittest.TestCase):
   def test_encode_0(self):
-    self.assertEqual(Lzw().encode('hehe'), [104, 101, 256])
-    self.assertEqual(Lzw().encode('ABBABBBABBA'), [
-                     65, 66, 66, 256, 257, 259, 65])
-    self.assertEqual(Lzw().encode('ABABA'), [65, 66, 256, 65])
-
+    self.assertEqual(Lzw().encode("hehe"), "heĀ")
+    self.assertEqual(Lzw().encode("ABABA"), "ABĀA")
+    self.assertEqual(Lzw().encode("ABBABBBABBA"), "ABBĀāăA")
 
 if __name__ == '__main__':
   unittest.main()
--- zaf/lzw.py
+++ zaf/lzw.py
@@ -1,28 +1,28 @@
 class Lzw:
-  def __init__(self):
-    self.dict = {chr(n): n for n in range(0, 256)}
+  def create_enc_dict(self):
+    return {chr(n): n for n in range(0, 256)}
 
   def encode(self, input):
+    dict = self.create_enc_dict()
     seq = input[0]
     code = 255
     res = []
     for i in range(1, len(input)):
       current_char = input[i]
-      if seq + current_char in self.dict:
+      if seq + current_char in dict:
         seq += current_char
       else:
-        res.append(self.dict[seq] if len(seq) > 1 else ord(seq))
+        res.append(dict[seq] if len(seq) > 1 else ord(seq))
         code += 1
-        self.dict[seq + current_char] = code
+        dict[seq + current_char] = code
         seq = current_char
 
-    res.append(self.dict[seq] if len(seq) > 1 else ord(seq))
-    return res
+    res.append(dict[seq] if len(seq) > 1 else ord(seq))
+    return ''.join(list(map(lambda n: chr(n), res)))
 
   def decode(self, input):
     return None
 
 if __name__ == "__main__":
-  lzw = Lzw()
-
-  print(lzw.encode("hehe"))
+  e = Lzw().encode("ABABA")
+  print(e)
