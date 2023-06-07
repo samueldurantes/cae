93d6561ab686757f3d1f41d5f452dea42014e85a
Samuel Durante <samueldurante.h@gmail.com>
2023-06-07 01:36:52 +0000

    add lzw decode function

--- test/test_lzw.py
+++ test/test_lzw.py
@@ -7,5 +7,26 @@ class TestLzw(unittest.TestCase):
     self.assertEqual(Lzw().encode("ABABA"), "ABĀA")
     self.assertEqual(Lzw().encode("ABBABBBABBA"), "ABBĀāăA")
 
-if __name__ == '__main__':
+  def test_decode_0(self):
+    lzw = Lzw()
+    str = "hehe"
+    enc = lzw.encode(str)
+
+    self.assertEqual(lzw.decode(enc), str)
+
+  def test_decode_1(self):
+    lzw = Lzw()
+    str = "ABABA"
+    enc = lzw.encode(str)
+
+    self.assertEqual(lzw.decode(enc), str)
+
+  def test_decode_2(self):
+    lzw = Lzw()
+    str = "ABBABBBABBA"
+    enc = lzw.encode(str)
+
+    self.assertEqual(lzw.decode(enc), str)
+
+if __name__ == "__main__":
   unittest.main()
--- zaf/lzw.py
+++ zaf/lzw.py
@@ -2,10 +2,13 @@ class Lzw:
   def create_enc_dict(self):
     return {chr(n): n for n in range(0, 256)}
 
+  def create_dec_dict(self):
+    return {n: chr(n) for n in range(0, 256)}
+
   def encode(self, input):
     dict = self.create_enc_dict()
     seq = input[0]
-    code = 255
+    code = 256
     res = []
     for i in range(1, len(input)):
       current_char = input[i]
@@ -13,16 +16,34 @@ class Lzw:
         seq += current_char
       else:
         res.append(dict[seq] if len(seq) > 1 else ord(seq))
-        code += 1
         dict[seq + current_char] = code
+        code += 1
         seq = current_char
 
     res.append(dict[seq] if len(seq) > 1 else ord(seq))
     return ''.join(list(map(lambda n: chr(n), res)))
 
   def decode(self, input):
-    return None
+    dict = self.create_dec_dict()
+    seq = out = input[0]
+    res = [out]
+    code = 256
+    for i in range(1, len(input)):
+      current_code = ord(input[i])
+      if current_code < code:
+        entry = dict[current_code]
+      else:
+        entry = seq + out
+
+      res.append(entry)
+      out = entry[0]
+      dict[code] = seq + out
+      code += 1
+      seq = entry
+    return ''.join(res)
 
 if __name__ == "__main__":
-  e = Lzw().encode("ABABA")
-  print(e)
+  lzw = Lzw()
+  e = lzw.encode("ABBABBBABBA")
+  d = lzw.decode(e)
+  print(d)
